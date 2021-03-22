2ae8b8dcbb9351bbca95c590b6eda591b0023301
Inan Xu <ixu2002new@gmail.com>
2021-03-22 14:29:26 -0700

    GA tracking rem debug

--- src/Tracking.js
+++ src/Tracking.js
@@ -1,9 +1,7 @@
 import ReactGA from 'react-ga';
 
 export const initGA = (trackingID) => {
-    ReactGA.initialize(trackingID, {
-        debug: true
-    });
+    ReactGA.initialize(trackingID);
 }
 export const PageView = () => {
   ReactGA.pageview(window.location.pathname + window.location.search);
